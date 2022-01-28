3e2fcc017f739ea4ec6162d4236c2d5989485533
Josh Froelich <joshcf@users.noreply.github.com>
2022-01-28 21:13:39 +0000

    Template of script in comments

---
+++ install.bash
@@ -0,0 +1,46 @@
+# Pi Home install script
+#
+# script to
+# 1 configure pi settings
+# 2 set static network config
+# 3 update pi
+# 4 install pi hole
+# 5 install my preffered block lists
+# 6 setup wireguard wg0
+# 7 setup wg-cli
+# 8 Add facebook asn firewall block to wg0
+# 9 setup wireguard wg1 (no firewalling)
+# 10 harden pi
+# 11 setup auto updating
+
+
+
+# function to configure pi
+
+
+# function to setup networking
+
+
+# function to update pi
+
+
+# function to install pihole
+
+
+# function to setup blocklists
+
+
+# function to setup wg0
+
+
+# function to setup facebook firewall block
+
+
+# function to setup wg1
+
+
+# function to harden pi
+
+
+# function to setup auto updates
+
